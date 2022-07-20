6c85a37fdb527cc8bdac9e4f3d7f24d1a9f61083
Mahmoud2002d <wa7wa776@gmail.com>
2022-07-20 14:10:13 +0200

    Version 7

--- js/main.js
+++ js/main.js
@@ -8,4 +8,25 @@ function fading() {
   $(about).toggle("display");
   $(projects).toggle("display");
 }
-
+// change theme colors
+$("#gear").on("click", function () {
+  $(".theme-changer").toggleClass("leftside");
+});
+$(".theme-changer ul li i").on("click", function () {
+  $("body").css({ "background-color": $(this).data("color") });
+  if ($(this).data("color") == "black") {
+    $(":root").css("--black-color", "white");
+    $(":root").css("--white-color", "black");
+    $(".text h1::after").css("background-color", "white");
+    $("html").css("color", "white");
+    $(":root").css("--shadow-color", "rgba(255, 255, 255, 0.1)");
+    $(":root").css("--second-color", "white");
+  } else if ($(this).data("color") == "white") {
+    $(":root").css("--black-color", "black");
+    $(":root").css("--white-color", "white");
+    $(".text h1::after").css("background-color", "black");
+    $("html").css("color", "black");
+    $(":root").css("--shadow-color", "rgba(0, 0, 0, 0.1)");
+    $(":root").css("--second-color", "#1a5276");
+  }
+});
